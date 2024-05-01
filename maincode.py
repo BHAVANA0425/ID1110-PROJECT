f60fd7c2bb54508558633dec8e6c64a272d077d7
BHAVANA0425 <168323713+BHAVANA0425@users.noreply.github.com>
2024-05-01 14:01:38 +0530

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -50,3 +50,58 @@ def save_expense_to_file(expense:Expense, expense_file_path):
     with open(expense_file_path, "a") as f:
         f.write(f"{expense.name},{expense.category}, {expense.amount}\n")
 
+def summarize_expenses(expense_file_path, budget):
+    print(f"Summarizing user Expense")
+    expenses: list[Expense] = []
+    with open(expense_file_path, "r") as f:
+        lines = f.readlines()
+        for line in lines:
+            stripped_line = line.strip()
+            expense_name, expense_category, expense_amount = line.strip().split(",")
+            print(expense_name, expense_category, expense_amount)
+            line_expense = Expense(name=expense_name, category=expense_category, amount=float(expense_amount))
+
+            expenses.append(line_expense)
+
+    amount_by_category = {}
+    for expense in expenses:
+        key = expense.category
+        if key in amount_by_category:
+            amount_by_category[key] += expense.amount
+        else:
+            amount_by_category[key] = expense.amount
+    print("Expenses by category")
+    for key, amount in amount_by_category.items():
+        print(f"{key}: ${amount:.2f}")
+
+    total_spent = sum([x.amount for x in expenses])
+    print(f"you've spent $({total_spent:.2f})this month!")
+
+    remaining_budget = budget - total_spent
+    print(f"budget remaining: ${remaining_budget:.2f}")
+
+    #get the current date
+    now = datetime.datetime.now()
+
+    #get the number of days in the current month
+    days_in_month = calendar.monthrange(now.year, now.month)[1]
+
+    #calculate the remaining number of days in the current month
+    remaining_days = days_in_month - now.day
+
+    print("Remaining days in the current month:", remaining_days)
+
+
+    if remaining_days==0:
+        print(f"daily budget=={remaining_budget}")
+    else:
+        daily_budget = remaining_budget / remaining_days
+        print(f"Budget per day: {daily_budget:.2f}")
+
+
+
+
+if __name__ == "__main__":
+    main()
+
+
