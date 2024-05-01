b139eb128bc6f0c0f90bb52548fc09f1099df585
Kiranjyothi1340 <168324920+Kiranjyothi1340@users.noreply.github.com>
2024-05-01 13:58:26 +0530

    Update maincode.py

--- maincode.py
+++ maincode.py
@@ -19,3 +19,34 @@ def main():
 
     #read file and summarize expenses
     summarize_expenses(expense_file_path, budget)
+def get_user_expense():
+    print(f"Getting User Expense")
+    expense_name = input("Enter your Expense Name: ")
+    expense_amount = float(input("Enter your Expense Amount: "))
+    expense_categories = ["Food","Home","Work","Sport","Fun","misc"]
+
+    while True:
+        print("select a category: ")
+        for i, category_name in enumerate(expense_categories):
+            print(f"{i+1}.{category_name}")
+        value_range = f"[1-{len(expense_categories)}]"
+
+        selected_index = int(input(f"Enter a category number {value_range}:"))-1
+
+        if i in range(len(expense_categories)):
+            selected_category = expense_categories[selected_index]
+            new_expense = Expense(name=expense_name, category=selected_category, amount=expense_amount)
+            return new_expense
+
+        else:
+            print("invalid choice.please try again")
+
+
+
+
+
+def save_expense_to_file(expense:Expense, expense_file_path):
+    print(f"saving user Expense:{expense} to {expense_file_path}")
+    with open(expense_file_path, "a") as f:
+        f.write(f"{expense.name},{expense.category}, {expense.amount}\n")
+
